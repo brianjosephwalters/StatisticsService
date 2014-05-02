40b25f39ada608e98cf9b39515d56f13e59b0d90
Brian J. Walters <bwalters@uno.edu>
2014-05-01 19:32:58 -0500

    Uh oh

--- build/generated-sources/jax-ws/com/statisticsservice/courseservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://courseservice.com/")
-package com.statisticsservice.courseservice;
--- build/generated/jax-wsCache/DOLService/com/statisticsservice/dolservices/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://dol.com/")
-package com.statisticsservice.dolservices;
